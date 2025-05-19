b5480ffbe907a8263c2d8f30ef5e64ec711ab589
localuser <localuser@laptop.local>
2025-05-19 13:47:45 +0700

    Menambahkan materi pada sesi 00 - Template/Main.py, untuk variasi comment multiline

--- Sesi 00 - Template/Main.py
+++ Sesi 00 - Template/Main.py
@@ -2,3 +2,7 @@ print("helo dunia!!!!!!!")
 print("apa kabar kalian?")
 print("instalasi berhasil")
 print('"Menambahkan variasi contoh print "')
+print('"Menambahkan variasi contoh print "')
+"""Menambhakan
+ comment 
+ multinline"""
